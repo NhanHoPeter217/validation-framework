aeb613f901e0d6f910580616427b32b4675c3a19
southsea2410 <ddhnam21@clc.fitus.edu.vn>
2024-12-29 17:02:17 +0700

    feat(form-hooks): demo code

--- form-app/src/App.tsx
+++ form-app/src/App.tsx
@@ -1,13 +1,13 @@
 import { Toaster } from 'validation-framework';
 
 import 'validation-framework/dist/bundle.css';
+import TestForm from './testform';
 import BasicSchemaValidation from './basic-form';
 
 function App() {
   return (
     <main className="h-screen w-screen flex flex-col items-center justify-center">
       <div className="container">
-        {/* <Form /> */}
         <BasicSchemaValidation />
       </div>
       <Toaster />
--- form-app/src/basic-form.tsx
+++ form-app/src/basic-form.tsx
@@ -2,55 +2,54 @@ import { ErrorOutlet, useForm, v } from 'validation-framework';
 
 const validationSchema = v
   .object({
-    firstName: v.string().required(),
-    lastName: v.string().max(5).required(),
+    firstName: v.string().required().email().nonEmpty().lowercase(),
+    lastName: v
+      .string()
+      .max(20)
+      .required()
+      .addTest({
+        name: 'My custom test',
+        exclusive: true,
+        test(value) {
+          if (value === 'my custom test') return true;
+          return false;
+        },
+        message: 'Failing Custom test'
+      }),
     min: v.number().gte(10),
     max: v.number().lte(20),
     minDate: v.date().min('2019-08-01'),
-    maxDate: v.date().max('2019-08-01'),
+    maxDate: v.date().max(new Date()),
     minLength: v.string().min(2),
     minRequiredLength: v.string().min(2).required(),
-    // selectNumber: v.string().required(),
-    pattern: v.string().matches(/\d+/)
-    // radio: v.string().required(),
-    // checkbox: v.string().required()
+    regexPattern: v.string().matches(/\d+/)
   })
   .required();
 
 type FormValues = v.infer<typeof validationSchema>;
 
 const BasicSchemaValidation: React.FC = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors }
-  } = useForm<FormValues>({
+  const { register, handleSubmit } = useForm<FormValues>({
     schema: validationSchema,
     errorDisplays: ['outlet']
   });
 
   const onSubmit = (data: FormValues) => {
     console.log('Submitted Value', data);
+    alert('Submitted successfully: ' + JSON.stringify(data));
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register('firstName')} placeholder="firstName" />
-      {errors.firstName && <p>firstName error</p>}
-      <input {...register('lastName')} placeholder="lastName" />
-      {errors.lastName && <p>lastName error</p>}
+      <input {...register('firstName')} placeholder="Kết hợp các validation với nhau cho cùng kiểu dữ liệu" />
+      <input {...register('lastName')} placeholder="Custom Validation" />
+      <input {...register('regexPattern')} placeholder="Regex Pattern" />
       <input type="number" {...register('min')} placeholder="min" />
-      {errors.min && <p>min error</p>}
       <input type="number" {...register('max')} placeholder="max" />
-      {errors.max && <p>max error</p>}
       <input type="date" {...register('minDate')} placeholder="minDate" />
-      {errors.minDate && <p>minDate error</p>}
       <input type="date" {...register('maxDate')} placeholder="maxDate" />
-      {errors.maxDate && <p>maxDate error</p>}
       <input {...register('minLength')} placeholder="minLength" />
-      {errors.minLength && <p>minLength error</p>}
       <input {...register('minRequiredLength')} placeholder="minRequiredLength" />
-      {errors.minRequiredLength && <p>minRequiredLength error</p>}
       <button type="submit">Submit</button>
       <ErrorOutlet />
     </form>
